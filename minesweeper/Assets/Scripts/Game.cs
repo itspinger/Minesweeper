64b3ab3c703108741c37d9c29d9cb5f38ad27281
Dimitrije Mijailovic <pinger.business@gmail.com>
2022-04-08 23:15:31 +0200

    Started working on the Game logic

---
+++ minesweeper/Assets/Scripts/Game.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private int _height;
+    private int _width;
+    private int _mines;
+    
+    private readonly Field[,] _fields;
+
+    public void Start()
+    {
+        // Load all the necessary game data
+    }
+}
