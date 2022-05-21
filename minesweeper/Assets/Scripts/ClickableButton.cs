eceb16e97cbedafd532ef187ba891db752f001f5
Dimitrije Mijailovic <pinger.business@gmail.com>
2022-05-21 16:19:22 +0200

    Edited the ClickableButton to handle the right click better

--- minesweeper/Assets/Scripts/ClickableButton.cs
+++ minesweeper/Assets/Scripts/ClickableButton.cs
@@ -8,7 +8,7 @@ using UnityEngine.Events;
  * makes it easier to use than the Unity default Button implementation 
  */
 
-public class ClickableButton : MonoBehaviour, IPointerClickHandler
+public class ClickableButton : MonoBehaviour, IPointerClickHandler, IPointerDownHandler
 {
     public UnityEvent OnRightClick;
     public UnityEvent OnLeftClick;
@@ -24,12 +24,22 @@ public class ClickableButton : MonoBehaviour, IPointerClickHandler
         // Perform the first event
         if (eventData.button == PointerEventData.InputButton.Right)
         {
-            OnRightClick.Invoke();
             return;
         }
 
         // This means that it's the left click
         // So perform the event
         OnLeftClick.Invoke();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // If it's the right click
+        // Perform the first event
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightClick.Invoke();
+            return;
+        }
     }
 }
